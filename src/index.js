82010280741db36346c454cb2fe407289236c8c7
ihtishamm <ihtishamhassan1111@gmail.com>
2024-04-13 15:21:26 +0000

    implement api error handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
@@ -8,6 +9,18 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8001, () => {
+        console.log(`Server is running at port:  ${process.env.PORT}`)
+    })
+    app.on("error", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+     })
+})
+.catch((err) => {
+    console.log("Mongodb connection faild !!!",err)
+})
 
 
 
