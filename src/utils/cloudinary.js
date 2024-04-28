6236c44838f44450d13d71e51471dca51ecf1755
ihtishamm <ihtishamhassan1111@gmail.com>
2024-04-28 12:01:59 +0500

    write simple controller for user registration

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -28,7 +28,3 @@ import { v2  as cloudinary} from "cloudinary";
   }
 
     export default uploadOnCloudinary()
-
-//  cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-//  { public_id: "olympic_flag" }, 
-//  function(error, result) {console.log(result); });
