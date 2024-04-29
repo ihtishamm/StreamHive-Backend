a0141d95b63fa02d782ca112358f8cd6e68c1f24
ihtishamm <ihtishamhassan1111@gmail.com>
2024-04-29 23:16:46 +0500

    refactor the code

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors({
 
 }))
 app.use(express.json({
-    limit:"300kb"
+    limit:"16kb"
 }))
 
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -50,7 +50,7 @@ const registerUser = asyncHandler( async (req, res) => {
   const avatar = await uploadOnCloudinary(avatarLocalPath)
   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-   console.log("avatar", avatar)
+
   if (!avatar) {
       throw new ApiError(400, "Avatar file is required")
   }
