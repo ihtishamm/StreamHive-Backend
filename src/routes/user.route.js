774ef1f50bebdc65b1d54a8015b00ba9ec8f0656
ihtishamm <ihtishamhassan1111@gmail.com>
2024-04-29 22:38:23 +0500

    fix issues to upload the file on the cloudinary

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,18 +3,19 @@ import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router = Router()
 
- router.route("/register").post(
-     upload.fields([
+router.route("/register").post(
+    upload.fields([
         {
             name: "avatar",
-            maxCount:1
-        },
-         {
-            name:"coverImage",
-            maxCount:1
-         }
-     ]),
-    registerUser)
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
 
 export default router
