3a8c2ac9d62de4ba9849707e3b8080ecffd97413
ihtishamm <ihtishamhassan1111@gmail.com>
2024-05-10 00:11:58 +0500

    updateUserAVatar Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -242,5 +242,32 @@ const loginUser = asyncHandler(async (req, res) =>{
             .json(new ApiResponse(200, user, "Account details updated successfully"))
     })   
 
+     const updateUserAvatar = asyncHandler(async(req, res) => {
+           
+       const avatarLocalPath = req.file?.path 
+       
+        if(!avatarLocalPath){
+          throw new ApiError(400, "Avatar file is missing")
+        }
 
-export { registerUser, loginUser, logoutUser, RefreshAccessToken,changeCurrentPassword, getCurrentUser, updateAccountDetails};
+        const avatar = await uploadOnCloudinary(avatarLocalPath)
+        if(!avatar.url){
+          throw new ApiError(400, "Error while uploading on Cloudinary")
+        }
+         await User.findByIdAndUpdate(
+          req.user?._id,
+          {
+            $set: {
+              avatar: avatar.url
+            }
+          },
+           {new: true}
+         ).select("-password")
+     })
+
+
+
+
+export { registerUser, loginUser, logoutUser, 
+  RefreshAccessToken,changeCurrentPassword, getCurrentUser,
+   updateAccountDetails,updateUserAvatar};
