bbabdcdfc422fa2183017b0eeb9ba650db2ce898
ihtishamm <ihtishamhassan1111@gmail.com>
2024-05-11 14:19:59 +0500

    add routes for update user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, RefreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, RefreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -24,5 +24,8 @@ router.route("/register").post(
   router.route("/logout").post(verifyJWT, logoutUser)
    
    router.route("/refreshToken").post(RefreshAccessToken)
-
+    router.route("/changePassword").post(verifyJWT, changeCurrentPassword)
+    router.route("/currentUser").get(verifyJWT, getCurrentUser)
+    router.route("/updateAccount").patch(verifyJWT, updateAccountDetails)
+    router.route("/updateAvatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar)
 export default router
